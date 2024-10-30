d8fa15ab37c57485a73d41115d477d1a4030b32a
Stefan Smiljić <133028410+stefansmiljic@users.noreply.github.com>
2024-10-30 21:41:38 +0100

    Basic logic implemented

---
+++ timer/src/Timer.tsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from "react"
+interface TimerProps{title:string, endTime:number, elapsedTime?:number}
+
+const Timer = ({title, endTime, elapsedTime}: TimerProps)=>{
+
+    const difference = elapsedTime!=null ? endTime-elapsedTime : endTime
+    
+    const [timeLeft, setTimeLeft] = useState<number>(difference)
+    const [elapsed, setElapsed] = useState<number>(elapsedTime!=null?elapsedTime:0)
+    const [isStarted, setIsStarted] = useState<boolean>(false)
+
+    const elapsedTimeMinutes = Math.floor(elapsed/60).toString().padStart(2, "0")
+    const elapsedTimeSeconds = (elapsed%60).toString().padStart(2, "0")
+    const minutesLeft = Math.floor(timeLeft/60).toString().padStart(2, "0")
+    const secondsLeft = (timeLeft%60).toString().padStart(2, "0")
+
+    useEffect(()=>{
+        let interval:NodeJS.Timeout
+        if(isStarted && timeLeft>0){
+            interval = setInterval(()=>{
+                setTimeLeft((time)=>time-1)
+                setElapsed((time)=>time+1)
+            }, 1000)
+        }
+        else if(timeLeft==0){
+            setIsStarted(false)
+        }
+        return ()=>clearInterval(interval)
+    }, [isStarted])
+
+    function startHandle(){
+        setIsStarted(true)
+    }
+    function resetHandle(){
+        setIsStarted(false)
+        setTimeLeft(endTime)
+        setElapsed(0)
+    }
+    function pauseHandle(){
+        setIsStarted(false)
+    }
+
+    return <div>
+        <p>{title}</p>
+        <p>{elapsedTimeMinutes}:{elapsedTimeSeconds}</p>
+        <p>{minutesLeft}:{secondsLeft}</p>
+        <button onClick={startHandle}>Start</button>
+        <button onClick={pauseHandle}>Pause</button>
+        <button onClick={resetHandle}>Reset</button>
+    </div>
+}
+export default Timer
